69e5eef0a69bf53a71a13a5a7eff5242d2591327
preethi200027 <chennapreethi.27@gmail.com>
2021-11-08 23:09:37 +0530

    java

---
+++ operators/arthematic.java
@@ -0,0 +1,12 @@
+class operator{
+}
+public static void main( string args[]){
+    int a=20;
+    int b=10;
+    System.out.println("a+b="+(a+b));
+    System.out.println("a-b="+(a-b));
+    System.out.println("a*b="+(a*b));
+    System.out.println("a/b="+(a/b));
+    System.out.println("a%b="+(a%b));} 
+
+}
---
+++ operators/operator/arthematic.java
@@ -0,0 +1,20 @@
+
+public class arithop {
+ public static void main(String[] args) {
+  //Variables Definition and Initialization
+  int number1 = 12, number2 = 4;
+
+  //Addition Operation
+  int sum = number1 + number2;
+  System.out.println("Sum is: " + sum);
+
+  //Subtraction Operation
+  int dif = number1 - number2;
+  System.out.println("Difference is : " + dif);
+
+  //Multiplication Operation
+  int mul = number1 * number2;
+  System.out.println("Multiplied value is : " + mul);
+
+ }
+}
